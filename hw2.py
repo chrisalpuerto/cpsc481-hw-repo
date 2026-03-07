9b6a0a9f2082431930060fdf81e0ee339dec8536
chris <bbhriss@gmail.com>
2026-03-06 19:57:39 -0800

    1st commit

---
+++ hw2.py
@@ -0,0 +1,11 @@
+# HW 2
+# Chris Alpuerto
+
+# 1. Write a function that takes in a list of numbers and returns the sum of the numbers in the list.
+
+heuristic = {
+    "Arad": 366,
+    "Sibiu": 253,
+    "Fagaras": 176,
+    "Bucharest": 0
+}
